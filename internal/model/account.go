48704b96489aadc642c4aa6ef71b80ac22ac0aaf
Ruslan Semagin <pixel.365.24@gmail.com>
2025-02-13 13:13:24 +0300

    fix: add missing 'updated_at' fields
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

--- internal/model/account.go
+++ internal/model/account.go
@@ -7,6 +7,7 @@ import (
 
 type Account struct {
 	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
 	Login     string        `json:"login"`
 	Cookies   []http.Cookie `json:"cookies"`
 }
--- internal/model/module.go
+++ internal/model/module.go
@@ -4,6 +4,7 @@ import "time"
 
 type Module struct {
 	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 	Name        string    `json:"name"`
 	Path        string    `json:"path"`
 	Description string    `json:"description"`
