bd5e541513e5da3eb998fa27dae3d82ce0801701
Ruslan Semagin <pixel.365.24@gmail.com>
2025-02-13 14:02:20 +0300

    feat(account): add ls subcommand
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

--- cmd/account/ls.go
+++ cmd/account/ls.go
@@ -1,19 +1,38 @@
 package account
 
 import (
-	"errors"
+	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"github.com/pixel365/bx/internal/config"
 )
 
 func lsCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "ls",
 		Short: "List accounts",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			return errors.New("not implemented")
+		RunE: func(c *cobra.Command, _ []string) error {
+			conf, err := config.GetConfig()
+			if err != nil {
+				return err
+			}
+
+			if len(conf.Accounts) == 0 {
+				fmt.Println("No accounts found")
+				return nil
+			}
+
+			verbose, _ := c.Flags().GetBool("verbose")
+			for _, acc := range conf.Accounts {
+				acc.PrintSummary(verbose)
+			}
+
+			return nil
 		},
 	}
+
+	cmd.Flags().BoolP("verbose", "v", false, "Show extended information")
 
 	return cmd
 }
--- internal/model/account.go
+++ internal/model/account.go
@@ -1,8 +1,11 @@
 package model
 
 import (
+	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 type Account struct {
@@ -10,4 +13,15 @@ type Account struct {
 	UpdatedAt time.Time     `json:"updated_at"`
 	Login     string        `json:"login"`
 	Cookies   []http.Cookie `json:"cookies"`
+}
+
+func (a *Account) PrintSummary(verbose bool) {
+	if verbose {
+		color.Green("Login: %s", a.Login)
+		fmt.Printf("Created At: %s\n", a.CreatedAt)
+		fmt.Printf("Updated At: %s\n", a.UpdatedAt)
+		fmt.Printf("Is Authenticated: %t\n", len(a.Cookies) > 0)
+	} else {
+		fmt.Printf("%s\n", a.Login)
+	}
 }
