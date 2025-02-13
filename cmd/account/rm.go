cee145762dbefcb0326277b17dbcb5a6662b2e4d
Ruslan Semagin <pixel.365.24@gmail.com>
2025-02-13 17:11:37 +0300

    feat(account): rm subcommand
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

--- cmd/account/rm.go
+++ cmd/account/rm.go
@@ -1,9 +1,13 @@
 package account
 
 import (
-	"errors"
+	"fmt"
 
+	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
+
+	"github.com/pixel365/bx/internal/config"
+	"github.com/pixel365/bx/internal/model"
 )
 
 func rmCmd() *cobra.Command {
@@ -11,7 +15,59 @@ func rmCmd() *cobra.Command {
 		Use:   "rm",
 		Short: "Remove an account",
 		RunE: func(_ *cobra.Command, _ []string) error {
-			return errors.New("not implemented")
+			conf, err := config.GetConfig()
+			if err != nil {
+				return err
+			}
+
+			if len(conf.Accounts) == 0 {
+				fmt.Println("No accounts found")
+				return nil
+			}
+
+			login := ""
+			confirm := false
+
+			var options []huh.Option[string]
+			for _, a := range conf.Accounts {
+				options = append(options, huh.NewOption(a.Login, a.Login))
+			}
+
+			if err = huh.NewSelect[string]().
+				Title("Select the account you want to delete:").
+				Options(options...).
+				Value(&login).
+				Run(); err != nil {
+				return err
+			}
+
+			if err = huh.NewConfirm().
+				Title(fmt.Sprintf("Are you sure you want to delete %s?", login)).
+				Affirmative("Yes").
+				Negative("No").
+				Value(&confirm).
+				Run(); err != nil {
+				return err
+			}
+
+			if confirm {
+				var accounts []model.Account
+				for _, a := range conf.Accounts {
+					if a.Login != login {
+						accounts = append(accounts, a)
+					}
+				}
+
+				conf.Accounts = accounts
+
+				if err := conf.Save(); err != nil {
+					return err
+				}
+
+				fmt.Printf("Account %s was deleted.\n", login)
+			}
+
+			return nil
 		},
 	}
 
