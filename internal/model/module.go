e615c16d1cabe24d086499e40f96d71d6990cbd2
Ruslan Semagin <pixel.365.24@gmail.com>
2025-02-14 09:53:33 +0300

    add login field to module model
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

--- internal/model/module.go
+++ internal/model/module.go
@@ -8,4 +8,5 @@ type Module struct {
 	Name        string    `json:"name"`
 	Path        string    `json:"path"`
 	Description string    `json:"description"`
+	Login       string    `json:"login"`
 }
