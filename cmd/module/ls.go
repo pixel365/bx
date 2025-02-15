c307903d9e06e0bab11074b64fa274f8ef156da8
Ruslan Semagin <pixel.365.24@gmail.com>
2025-02-15 12:57:18 +0300

    feat(module): add `ls` subcommand
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

--- cmd/module/ls.go
+++ cmd/module/ls.go
@@ -1,7 +1,12 @@
 package module
 
 import (
-	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/pixel365/bx/internal"
+
+	"github.com/pixel365/bx/internal/config"
 
 	"github.com/spf13/cobra"
 )
@@ -10,12 +15,60 @@ func lsCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "ls",
 		Short: "List available modules",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			return errors.New("not implemented")
+		RunE: func(c *cobra.Command, _ []string) error {
+			conf, err := config.GetConfig()
+			if err != nil {
+				return err
+			}
+
+			if len(conf.Modules) == 0 {
+				fmt.Println("No modules found")
+				return nil
+			}
+
+			verbose, _ := c.Flags().GetBool("verbose")
+			all, _ := c.Flags().GetBool("all")
+			login, _ := c.Flags().GetString("login")
+			login = strings.TrimSpace(login)
+
+			if all {
+				login = ""
+			} else {
+				if login == "" {
+					if err = internal.ChooseAccount(&conf.Accounts, &login,
+						"Select the account whose modules you want to see:"); err != nil {
+						return err
+					}
+				}
+			}
+
+			if login != "" {
+				for _, mod := range conf.Modules {
+					mod.PrintSummary(verbose)
+				}
+			} else {
+				j := 0
+				for _, mod := range conf.Modules {
+					if mod.Login != login {
+						continue
+					}
+
+					j++
+					mod.PrintSummary(verbose)
+				}
+
+				if j == 0 {
+					fmt.Println("No modules found")
+				}
+			}
+
+			return nil
 		},
 	}
 
 	cmd.Flags().BoolP("verbose", "v", false, "Show extended information")
+	cmd.Flags().BoolP("all", "a", false, "Show all available modules")
+	cmd.Flags().BoolP("login", "l", false, "Login whose modules need to be shown")
 
 	return cmd
 }
