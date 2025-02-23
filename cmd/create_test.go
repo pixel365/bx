bace409775e298e4399520574f11454cba86deea
Ruslan Semagin <pixel.365.24@gmail.com>
2025-02-23 17:13:34 +0300

    test: add test for `create` subcommand
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

---
+++ cmd/create_test.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func Test_newCreateCommand(t *testing.T) {
+	cmd := newCreateCommand()
+
+	t.Run("", func(t *testing.T) {
+		if cmd == nil {
+			t.Error("nil cmd")
+		}
+
+		if cmd.Use != "create" {
+			t.Errorf("cmd.Use should be 'create' but got '%s'", cmd.Use)
+		}
+
+		if len(cmd.Aliases) != 1 {
+			t.Errorf("len(cmd.Aliases) should be 1 but got %d", len(cmd.Aliases))
+		}
+
+		if cmd.Aliases[0] != "c" {
+			t.Errorf("cmd.Aliases[0] should be 'c' but got '%s'", cmd.Aliases[0])
+		}
+
+		if cmd.Short != "Create a new module" {
+			t.Errorf("cmd.Short should be 'Create a new module' but got '%s'", cmd.Short)
+		}
+
+		if !cmd.HasFlags() {
+			t.Errorf("cmd.HasFlags() should be true")
+		}
+	})
+}
