65eca3ff9ec368174964a908e82460d6a6345a79
Ruslan Semagin <pixel.365.24@gmail.com>
2025-03-01 09:31:58 +0300

    Test (#39)
    test: newPushCommand
    
    Signed-off-by: Ruslan Semagin <pixel.365.24@gmail.com>

---
+++ cmd/push_test.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func Test_newPushCommand(t *testing.T) {
+	t.Run("should create new push command", func(t *testing.T) {
+		cmd := newPushCommand()
+		if cmd == nil {
+			t.Error("new push command should not be nil")
+		}
+
+		if cmd.Use != "push" {
+			t.Errorf("new push command should use 'push', got '%s'", cmd.Use)
+		}
+
+		if cmd.Short != "Push module to a Marketplace" {
+			t.Errorf("cmd.Short should be 'Push module to a Marketplace' but got '%s'", cmd.Short)
+		}
+
+		if cmd.RunE == nil {
+			t.Error("cmd RunE should not be nil")
+		}
+
+		if !cmd.HasFlags() {
+			t.Errorf("cmd.HasFlags() should be true")
+		}
+
+		if cmd.HasSubCommands() {
+			t.Errorf("cmd.HasSubCommands() should be false")
+		}
+
+		if len(cmd.Aliases) > 0 {
+			t.Errorf("len(cmd.Aliases) should be 0 but got %d", len(cmd.Aliases))
+		}
+	})
+}
